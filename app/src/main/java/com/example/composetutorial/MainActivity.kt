e7d7ae3e4181bbdaf161a8fd8d4fe0e2ac527bb3
Ant Armando Junior <elfiservice@hotmail.com>
2022-11-20 08:59:08 -0300

    feat : moved Message to model package and did some adjust 2

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -3,19 +3,17 @@ package com.example.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import com.example.composetutorial.model.Message
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,8 +23,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
@@ -45,7 +41,7 @@ fun MessageCard(msg: Message) {
 
         Column {
             Text(text = msg.author)
-            Spacer(modifier = Modifier.width(4.dp))
+            Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
     }
